80354d620727659fef91ae49857203afceb9994c
JakeSalter-Coder <jsalty15@gmail.com>
2024-09-11 00:04:29 -0500

    09/11 Jake Salter -Added a 'quit' option for flask server -Added stylesheet dir(empty for now) -Added basic header and form to the website

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
 from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/<name>')
 def index(name=None):
     return render_template('index.html', name=name)
 
+
+@app.route("/quit")
+def _quit():
+    exit(0)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=3240)
