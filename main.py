f02c2138f7c43fd7df422e36cbff110c2babbce4
ronaldjm2 <88257932+ronaldjm2@users.noreply.github.com>
2024-11-05 15:27:18 -0600

    Added a selector for existing diseases, a consent form, and connected the data to the database.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from operator import truediv
 
 from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
@@ -6,59 +7,100 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Abacab#2024'
-app.config['MYSQL_DB'] = 'BOOKS_DB'
+app.config['MYSQL_PASSWORD'] = 'Superpie1@'
+app.config['MYSQL_DB'] = 'Final_Project'
 app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_CONNECT_TIMEOUT'] = 60
 
 mysql = MySQL(app)
 
-def connect():
+def insert_new_patients(user_input):
     try:
         # Create a cursor object
         conn = mysql.connection
-        if conn == None:
+        if conn is None:
             print("Issue with connection")
+            return
         cur = conn.cursor()
-        
-        # Insert data into the database
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", ('Derek', 'Credland'))
-        
-        # Commit changes
-        conn.commit()
-        cur.close()
-        conn.close()
+        # Grabs the number of patients logged to find the new patients ID
+        cur.execute("SELECT COUNT(*) FROM User_Information")
+        patient_id = f"auth_{cur.fetchone()[0]+1}"
 
+        # Grabs disease ID from disease name
+        cur.execute(f"SELECT Disease_ID FROM Disease WHERE Disease_Name='{user_input['disease']}'")
+        disease_id = cur.fetchone()[0]
+
+        # Determines if the user is obese, based on height and weight
+        is_obese = check_obese(user_input["height"], user_input["weight"])
+
+        # Converts height from inches to a formatted string in feet and inches (e.g., "5 ft 10 in").
+        formatted_height = user_input["height"]
+        formatted_height = f"{formatted_height // 12} ft {formatted_height % 12} in"
+
+        # Concat values into a single list
+        values = (patient_id,
+                  user_input['first_name'],
+                  user_input['last_name'],
+                  user_input['race'],
+                  user_input['weight'],
+                  formatted_height,
+                  user_input['gender'],
+                  is_obese,
+                  disease_id)
+
+        # Insert data into the database
+        insert = """INSERT INTO User_Information(Patient_ID, First_name, Last_name, 
+                                                 Race, Weight, Height, Gender, Obesity, Disease_ID) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        cur.execute(insert, values)
+        conn.commit()
     except Exception as e:
         print(f"Error: {e}")
 
 
+def check_obese(height_inches, weight_lbs):
+    height_meters = height_inches * 0.0254
+    weight_kilograms = weight_lbs * 0.453592
+    bmi = weight_kilograms / (height_meters ** 2)
+    if bmi >= 30:
+        return True
+    else:
+        return False
+
 @app.route('/')
 def index():
-    connc = mysql.connection
-    if connc == None:
+    # connect to db
+    conn = mysql.connection
+    if conn is None:
         print("Issue with connection")
-    cur = connc.cursor()
-    currently_logged_diseases = cur.execute("SELECT DISTINC disease_name FROM DISEASE")
-    return render_template('index.html', )
+    cur = conn.cursor()
+    # grab all logged diseases
+    cur.execute("SELECT DISTINCT disease_name FROM Disease")
+    currently_logged_diseases = cur.fetchall()
+    currently_logged_diseases = [row[0] for row in currently_logged_diseases]
+    return render_template('index.html', diseases=currently_logged_diseases)
 
 
 @app.route('/submit', methods=['POST'])
 def index_post():
     data = request.get_json()
 
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    age = data.get('age')
-    gender = data.get('gender')
-    height = data.get('height')
-    weight = data.get('weight')
-    race = data.get('race')
-    user_input = first_name + " " + last_name + " " + age + " " + gender + " " + height + " " + weight + " " + race
-    print(user_input)
+    # grab data from json object
+    user_input = {
+        "first_name": data.get('first_name'),
+        "last_name": data.get('last_name'),
+        "age": data.get('age'),
+        "gender": data.get('gender'),
+        "height": data.get('height_ft') * 12 + data.get('height_in'),
+        "weight": data.get('weight'),
+        "race": data.get('race'),
+        "disease": data.get('disease'),
+        "consent": data.get('consent')
+    }
     app.logger.info(user_input)
+    if user_input["disease"] != "None" and user_input["consent"] == True:
+        insert_new_patients(user_input)
 
-    connect()
-    
     response = {
         'status': 'success',
         'message': 'Data received successfully',
--- static/medical-website.interact.js
+++ static/medical-website.interact.js
@@ -1,47 +1,74 @@
-window.addEventListener("load", function() {
-    let submit_button = document.getElementById("user_input")
+jQuery(document).ready(function(){
+    jQuery(document).on("submit", "#user_input", function(){
+        event.preventDefault();
 
-submit_button.addEventListener("submit", (e) => {
-    e.preventDefault();
+        let user_first_name = jQuery('#first_name').val();
+        let user_last_name = jQuery('#last_name').val();
+        let user_age = jQuery('#age').val();
+        let user_gender;
+        if(document.getElementById("male").checked){
+            user_gender = "male";
+        } else if(document.getElementById("female").checked){
+            user_gender = "female";
+        } else{
+            user_gender = "other";
+        }
+        let user_height_ft = jQuery('#height_ft').val();
+        user_height_ft = parseInt(user_height_ft, 10);
+        let user_height_in = jQuery('#height_in').val();
+        user_height_in = parseInt(user_height_in, 10);
+        let user_weight = jQuery('#weight').val();
+        user_weight = parseFloat(user_weight);
+        let user_race = jQuery('#race').val();
+        let user_disease = jQuery('#disease').val()
+        let user_consent
+        if(jQuery('#consent-checkbox').length > 0) {
+            user_consent = jQuery('#consent-checkbox').prop('checked');
+        } else {
+            user_consent = 0;
+        }
+        if(user_first_name && user_last_name && user_age && user_gender && user_height_ft && user_height_in && user_weight && user_race) {
+            $.ajax({
+                url: '/submit',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    first_name: user_first_name,
+                    last_name: user_last_name,
+                    age: user_age,
+                    gender: user_gender,
+                    height_ft: user_height_ft,
+                    height_in: user_height_in,
+                    weight: user_weight,
+                    race: user_race,
+                    disease: user_disease,
+                    consent: user_consent
+                }),
+                success: function(response){
+                    alert('Data submit successfully: ' + JSON.stringify(response));
+                },
+                error: function(){
+                    alert('Error submitting data: ');
+                }
+            });
+        } else {
+            alert('Incorrect Input!');
+        }
+    })
 
-    let user_first_name = document.getElementById("first_name").value;
-    let user_last_name = document.getElementById("last_name").value;
-    let user_age = document.getElementById("age").value
-    let user_gender;
-    if(document.getElementById("male").checked){
-        user_gender = "male";
-    } else if(document.getElementById("female").checked){
-        user_gender = "female";
-    } else{
-        user_gender = "other";
-    }
-    let user_height = document.getElementById("height").value;
-    let user_weight = document.getElementById("weight").value;
-    let user_race = document.getElementById("race").value;
-
-    if(user_first_name && user_last_name && user_age && user_gender && user_height && user_weight && user_race) {
-        $.ajax({
-            url: '/submit',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                first_name: user_first_name,
-                last_name: user_last_name,
-                age: user_age,
-                gender: user_gender,
-                height: user_height,
-                weight: user_weight,
-                race: user_race
-            }),
-            success: function(){
-                alert('Data submit successfully: ' + JSON.stringify(response));
-            },
-            error: function(){
-                alert('Error submitting data: ');
+    jQuery('#disease').on('change', function(){
+        let selectedValue = jQuery(this).val();
+        if(selectedValue !== "None"){
+            if(jQuery('#consent-checkbox').length == 0){
+                jQuery('#consent').append (
+                    '<input type="checkbox" id="consent-checkbox" style="flex 0 0 auto;"/>' +
+                    '<label for="consent-checkbox" style="flex 1; text-align: left;">' +
+                        'I consent to have the information I provided added to the MTSU Center for Medical Database Management and Research database.' +
+                    '</label>'
+                )
             }
-        });
-    } else {
-        alert('Incorrect Input!');
-    }
-})
+        } else{
+            jQuery('#consent').empty();
+        }
+    })
 })
