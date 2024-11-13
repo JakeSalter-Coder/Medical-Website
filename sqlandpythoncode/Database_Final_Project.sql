ac5560057e8d72de6fc228c4a48621f66490cf68
ronaldjm2 <88257932+ronaldjm2@users.noreply.github.com>
2024-11-12 22:04:56 -0600

    Add files via upload

---
+++ sqlandpythoncode/Database_Final_Project.sql
@@ -0,0 +1,44 @@
+Create Schema if not exists Final_Project;
+
+use Final_Project;
+
+create table if not exists Disease
+(
+Disease_ID int,
+Disease_Name varchar(255),
+primary key (Disease_ID)
+);
+
+create table if not exists Synthetic_data
+(
+Patient_ID varchar(255),
+First_name varchar (255),
+Last_name varchar(255),
+Race varchar(255),
+Weight double,
+Height varchar(255),
+Gender varchar(255),
+Obesity bool,
+Disease_ID int,
+Age int,
+Lifestyle varchar(255),
+primary key (Patient_ID),
+Foreign key (Disease_ID) references Disease(Disease_ID)
+);
+
+create table if not exists User_Information
+(
+Patient_ID varchar(255),
+First_name varchar (255),
+Last_name varchar(255),
+Race varchar(255),
+Weight double,
+Height varchar(255),
+Gender varchar(255),
+Obesity bool,
+Disease_ID int,
+Age int,
+Lifestyle varchar(255),
+primary key (Patient_ID),
+Foreign key (Disease_ID) references Disease(Disease_ID)
+);
