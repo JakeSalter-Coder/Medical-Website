5193225282e4a02d37105c0e70c6e5ce24069474
ronaldjm2 <88257932+ronaldjm2@users.noreply.github.com>
2024-12-02 14:33:07 -0600

    Delete Final directory

--- Final/static/sidenav.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").classList.add("dimmed");
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").classList.remove("dimmed");
-}
